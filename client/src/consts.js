875eae2a1dad91442b71b4d9022d62c08ef69a0d
Vadim <vadimbaranivsky83@gmail.com>
2023-05-31 04:43:13 +0300

    -

--- client/src/consts.js
+++ client/src/consts.js
@@ -8,6 +8,6 @@ if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
   WEBSOCKET_API_URL = 'ws://localhost:5000'
 } else {
   API_URL = `https://${apiDomain}`
-  WEBSOCKET_API_URL = `ws://${apiDomain}`
+  WEBSOCKET_API_URL = `wss://${apiDomain}`
 }
 export {API_URL, WEBSOCKET_API_URL}
